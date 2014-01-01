3f50931d02b16e8786b94ff6a33ff951a1ccfd4d
bcoburn3 <bcoburn3@gmail.com>
2014-01-01 14:03:29 -0500

    add list flattening code

--- parse.py
+++ parse.py
@@ -34,9 +34,9 @@ def parselist(tokens):
                 if parencount == 0:
                     #we've found the relevant closing paren, copy this slice into the
                     #result list and update the index
-                    sublst = tokens[(i + 1):(endidx + 2)]
-                    res.append(parselist(sublst))
-                    print(sublst)
+                    sublist = tokens[(i + 1):(endidx + 2)]
+                    res.append(parselist(sublist))
+                    print(sublist)
                     i += endidx + 1
                     break
         else:
@@ -47,3 +47,64 @@ def parselist(tokens):
 print(parsestring("1 + ( 2 + ( 3 + 4 ) )"))
 #should print:
 #['1', '+', ['2', '+', ['3', '+', '4']]]
+
+def precedence(exp):
+    #returns the precendence of the lowest precedence operator in exp
+    #exp to be a list or character
+    if isinstance(exp, list):
+        res = []
+        if "+" in exp:
+            res.append(1)
+        if "-" in exp:
+            res.append(1)
+        if "*" in exp:
+            res.append(2)
+        if "/" in exp:
+            res.append(2)
+        return res
+    else:
+        if exp == "+":
+            return 1
+        elif exp == "-":
+            return 1
+        elif exp == "*":
+            return 2
+        elif exp == "/":
+            return 2
+
+def flattenlist(exp):
+    #takes an expresion represented as a list and recursively flattens it
+    last = len(exp) - 1
+    test = False
+    res = []
+    for idx, elem in enumerate(exp):
+        if isinstance(elem, list):
+            sublist = flattenlist(elem)
+            neighbors = []
+            if idx != 0:
+                neighbors.append(exp[idx - 1])
+            if idx != last:
+                neighbors.append(exp[idx + 1])
+            print("flatten test")
+            print(sublist)
+            print(neighbors)
+            print(precedence(sublist))
+            print(precedence(neighbors))
+            if min(precedence(sublist)) >= max(precedence(neighbors)):
+                #sub-expression can be flattened
+                res.extend(sublist)
+            else:
+                #sub-expression can't be flattened, just append as a list
+                res.append(sublist)
+        else:
+            #element isn't a list, so just append it
+            res.append(elem)
+    return res
+
+print(flattenlist(parsestring("1 + ( 2 + ( 3 + 4 ) )")))
+#should print:
+#['1', '+', '2', '+', '3', '+', '4']
+
+print(flattenlist(parsestring("1 + ( 2 + ( 3 + 4 ) ) * 5")))
+#should print:
+#['1', '+', ['2', '+', '3', '+', '4'], '*', '5']
